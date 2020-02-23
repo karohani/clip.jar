88208ff413efb5082ea951bfe036af73ffcd1b7a
karo <wilzilzor@gmail.com>
2020-02-23 13:48:54 +0900

    :bug

--- src/main/scala/Clip.scala
+++ src/main/scala/Clip.scala
@@ -1,23 +1,26 @@
 import java.io.File
-import java.awt.Toolkit
+import java.awt.{Graphics, Toolkit}
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.Transferable
-import java.awt.image.BufferedImage 
+import java.awt.image.{BufferedImage, ImageObserver}
+
 import javax.imageio.ImageIO
 import java.util.UUID
 
+import sun.awt.image.MultiResolutionCachedImage
+
 object Clip {
 
-  def genFileName (): String = {
-     UUID.randomUUID.toString // + ".png"
+  def genFileName(): String = {
+    UUID.randomUUID.toString // + ".png"
   }
 
-  def getClipboard (): Clipboard = {
+  def getClipboard(): Clipboard = {
     Toolkit.getDefaultToolkit().getSystemClipboard()
   }
 
-  def showClipboardMimeTypes (cpl: Clipboard) = {
+  def showClipboardMimeTypes(cpl: Clipboard) = {
     cpl.getAvailableDataFlavors().map(_.getPrimaryType()).toSet
   }
 
@@ -27,33 +30,42 @@ object Clip {
 
   /// Try to get a image from clipboard
   def getImage(cpl: Clipboard): Option[BufferedImage] = {
-    getContent(cpl)
+
+    val r = getContent(cpl)
       .filter(_.isDataFlavorSupported(DataFlavor.imageFlavor))
-      .map(_.getTransferData(DataFlavor.imageFlavor).asInstanceOf[BufferedImage])
+      .map(_.getTransferData(DataFlavor.imageFlavor).asInstanceOf[MultiResolutionCachedImage])
+      .map(image => {
+        val r: BufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB)
+        val bg: Graphics = r.createGraphics()
+        bg.drawImage(image, 0, 0, null)
+        bg.dispose()
+        r
+      })
+    r
   }
 
   /// Save image from clipboard to a file
-  def saveClipboardImage(directory: String, imageName: String){
+  def saveClipboardImage(directory: String, imageName: String) {
     val imgOpt = getImage(getClipboard())
-    // val ext    = filename.split("\\.").last
 
     imgOpt match {
-      case None      => println("Error: No image available in clipboard.")
+      case None => println("Error: No image available in clipboard.")
       case Some(img) => val file = new File(directory, imageName + ".png")
-                        ImageIO.write(img, "png", file)
-                        println(file.toString)
+        ImageIO.write(img, "png", file)
+        println(file.toString)
     }
   }
 
   /// Save image from clipboard to a file with extension png
-  def saveClipboardImageUUID(directory: String){
+  def saveClipboardImageUUID(directory: String) {
     // val imageName = (new File(directory,)).toString
     //println("File name = " + filename)      
-    saveClipboardImage(directory,  genFileName())
+    saveClipboardImage(directory, genFileName())
   }
 
-  def printHelp () {
-    println("""Usage:
+  def printHelp() {
+    println(
+      """Usage:
 
   Save clipboard image with a given Name
 
@@ -71,16 +83,17 @@ object Clip {
          - 415dafcf-5fd0-4d10-97f2-0da82bf1bf3f.png
 """)
 
-    }
+  }
 
 
   def main(args: Array[String]): Unit = {
+    args.map(println)
     args.toList match {
-      case List("--uuid", directory)           => saveClipboardImageUUID(directory)
-      case List("--name", name)                => saveClipboardImage(".", name)
-      case List("--name", name, directory)     => saveClipboardImage(directory, name)
-      case List()                              => printHelp()
-      case _                                   => println("Failed")
+      case List("--uuid", directory) => saveClipboardImageUUID(directory)
+      case List("--name", name) => saveClipboardImage(".", name)
+      case List("--name", name, directory) => saveClipboardImage(directory, name)
+      case List() => printHelp()
+      case _ => println("Failed")
 
     }
   } // End of main ()
